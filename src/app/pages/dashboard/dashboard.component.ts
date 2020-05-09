bed26baa1fdaf2ab6ba5e66f7196709a1ac75f97
Prabhath Bandara <okprabhath@gmail.com>
2020-05-09 10:33:39 +0530

    Viewer implemented

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -3,14 +3,21 @@ import {NbDialogService, NbToastrService} from '@nebular/theme';
 import {ApiService} from '../../@core/service/api-service/api.service';
 import {ErrorHandlingService} from '../../@core/service/error-handling.service';
 import {Project} from '../../@core/model/project';
+import {ProjectFile} from '../../@core/model/project-file';
 
 @Component({
-  selector: 'ngx-ecommerce',
+  selector: 'ngx-dashboard',
   templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
-  // @ViewChild('uploadBox', {static: false}) uploadBox: TemplateRef<any>;
+  @ViewChild('complexityViewer', {static: false}) complexityViewer: TemplateRef<any>;
   allProjects: Project[] = [];
+  sourcePathForNewScan: string = '';
+  projectKeyForNewScan: string = '';
+  loadingNewScan: boolean = false;
+  selectedProject: Project = null;
+  selectedFile: ProjectFile = null;
 
   constructor(private dialogService: NbDialogService,
               private toastrService: NbToastrService,
@@ -32,4 +39,42 @@ export class DashboardComponent implements OnInit {
     );
   }
 
+  startNewScan() {
+    this.loadingNewScan = true;
+    this.apiService.newScan(this.projectKeyForNewScan, this.sourcePathForNewScan).subscribe(
+      res => {
+        if (res) {
+          this.toastrService.success('Successfully scanned the project', 'Successful!');
+          this.sourcePathForNewScan = '';
+          this.projectKeyForNewScan = '';
+        } else {
+          this.toastrService.danger('Scanned failed', 'Failed!');
+        }
+        this.getAllProjects();
+        this.loadingNewScan = false;
+      },
+      err => {
+        ErrorHandlingService.handle(err, this.toastrService);
+        this.loadingNewScan = false;
+      },
+    );
+  }
+
+  selectProject(project: Project) {
+    this.selectedProject = project;
+  }
+
+  selectFile(file: ProjectFile) {
+    this.selectedFile = file;
+  }
+
+  getFileNameFromPath(relativePath: string) {
+    const pieces = relativePath.split(/[\s\\]+/);
+    return pieces[pieces.length - 1];
+  }
+
+  openComplexityViewerForFile(file: ProjectFile) {
+    this.selectFile(file);
+    this.dialogService.open(this.complexityViewer);
+  }
 }
