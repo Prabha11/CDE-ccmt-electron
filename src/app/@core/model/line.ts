c8e3c204ffd05b868248da91e2af924dcf1b9469
Prabhath Bandara <okprabhath@gmail.com>
2020-05-20 10:49:57 +0530

    new model components

--- src/app/@core/model/line.ts
+++ src/app/@core/model/line.ts
@@ -1,6 +1,9 @@
 import {VariableComplexity} from './variable-complexity';
 import {MethodComplexity} from './method-complexity';
 import {SizeComplexity} from './size-complexity';
+import {ControlStructureComplexity} from './control-structure-complexity';
+import {CouplingComplexity} from './coupling-complexity';
+import {InheritanceComplexity} from './inheritance-complexity';
 
 export interface Line {
   lineNo: number;
@@ -15,4 +18,7 @@ export interface Line {
   variableComplexity: VariableComplexity;
   methodComplexity: MethodComplexity;
   sizeComplexity: SizeComplexity;
+  controlStructureComplexity: ControlStructureComplexity;
+  couplingComplexity: CouplingComplexity;
+  inheritanceComplexity: InheritanceComplexity;
 }
